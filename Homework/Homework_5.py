606a5ecb6e1d4880337486ef9560bdd8c0e38ca6
selemenevpavel <selemenev-pavel@mail.ru>
2022-12-19 22:02:44 +0300

    Seminar 6

--- Homework/Homework_5.py
+++ Homework/Homework_5.py
@@ -61,7 +61,7 @@
 #     print('Победил первый игрок')
 
 
-# Игра против бота с ИНТЕЛЛЕКТОМ
+# # Игра против бота с ИНТЕЛЛЕКТОМ
 
 # from random import randint
 
@@ -194,47 +194,47 @@
 # # Входные и выходные данные хранятся в отдельных текстовых файлах.
 
 
-# with open('Homework\HW5.1.txt', 'w', encoding='UTF-8') as file:
-#     file.write(input('Текст для кодирования: '))
-# with open('Homework\HW5.1.txt', 'r') as file:
-#     text = file.readline()
+with open('Homework\HW5.1.txt', 'w', encoding='UTF-8') as file:
+    file.write(input('Текст для кодирования: '))
+with open('Homework\HW5.1.txt', 'r') as file:
+    text = file.readline()
 
 
-# def rle_encode(decoded_string):
-#     encoded_string = ''
-#     count = 1
-#     char = decoded_string[0]
-#     for i in range(1, len(decoded_string)):
-#         if decoded_string[i] == char:
-#             count += 1
-#         else:
-#             encoded_string = encoded_string + str(count) + char
-#             char = decoded_string[i]
-#             count = 1
-#             encoded_string = encoded_string + str(count) + char
-#     return encoded_string
+def rle_encode(decoded_string):
+    encoded_string = ''
+    count = 1
+    char = decoded_string[0]
+    for i in range(1, len(decoded_string)):
+        if decoded_string[i] == char:
+            count += 1
+        else:
+            encoded_string = encoded_string + str(count) + char
+            char = decoded_string[i]
+            count = 1
+            encoded_string = encoded_string + str(count) + char
+    return encoded_string
 
 
-# def rle_decode(encoded_string):
-#     decoded_string = ''
-#     char_amount = ''
-#     for i in range(len(encoded_string)):
-#         if encoded_string[i].isdigit():
-#             char_amount += encoded_string[i]
-#         else:
-#             decoded_string += encoded_string[i] * int(char_amount)
-#         char_amount = ''
-#     print(decoded_string)
+def rle_decode(encoded_string):
+    decoded_string = ''
+    char_amount = ''
+    for i in range(len(encoded_string)):
+        if encoded_string[i].isdigit():
+            char_amount += encoded_string[i]
+        else:
+            decoded_string += encoded_string[i] * int(char_amount)
+        char_amount = ''
+    print(decoded_string)
 
-#     return decoded_string
+    return decoded_string
 
 
-# with open('Homework\HW5.1.txt', 'r') as file:
-#     decoded_string = file.read()
+with open('Homework\HW5.1.txt', 'r') as file:
+    decoded_string = file.read()
 
-# with open('Homework\HW5.2.txt', 'w') as file:
-#     encoded_string = rle_encode(decoded_string)
-#     file.write(encoded_string)
+with open('Homework\HW5.2.txt', 'w') as file:
+    encoded_string = rle_encode(decoded_string)
+    file.write(encoded_string)
 
-# print('Закодированная строка: ' + rle_encode(decoded_string))
-# print('Декодированная строка: ' + decoded_string)
+print('Закодированная строка: ' + rle_encode(decoded_string))
+print('Декодированная строка: ' + decoded_string)
